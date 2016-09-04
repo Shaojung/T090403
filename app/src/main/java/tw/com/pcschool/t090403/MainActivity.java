4632bf07e53033ecb57185f12c064cbb7f225db0
Shaojung <shaojung@gmail.com>
2016-09-04 13:57:53 +0800

    Loading file from raw.

--- app/src/main/java/tw/com/pcschool/t090403/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t090403/MainActivity.java
@@ -3,8 +3,13 @@ package tw.com.pcschool.t090403;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
 
         File F2 = getCacheDir();
         Log.d("FILE", "Cache:" + F2.toString());
+    }
+    public void click1(View v)
+    {
+        InputStream is = null;
+        is = getResources().openRawResource(R.raw.animal);
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String inputLine = "";
+        try {
+            while ((inputLine = br.readLine()) != null) {
+                sb.append(inputLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d("READ", sb.toString());
+
     }
 }
