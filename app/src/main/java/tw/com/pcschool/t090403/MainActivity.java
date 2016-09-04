b6b7b89f48860136bc3a0a9d54d84d682302d156
Shaojung <shaojung@gmail.com>
2016-09-04 13:23:39 +0800

    Show FilesDir and Cache Dir.

--- app/src/main/java/tw/com/pcschool/t090403/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t090403/MainActivity.java
@@ -2,6 +2,9 @@ package tw.com.pcschool.t090403;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        File F1 = getFilesDir();
+        Log.d("FILE", "File:" + F1.toString());
+
+        File F2 = getCacheDir();
+        Log.d("FILE", "Cache:" + F2.toString());
     }
 }
